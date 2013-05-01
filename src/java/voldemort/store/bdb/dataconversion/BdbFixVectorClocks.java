9c4d874b697b35e737504b0c68f677b26fb9b5be
Abhinay Nagpal <abhinaynagpal@gmail.com>
2013-05-01 12:55:03 -0700

    Added utility to purge duplicates and write to a new destination we resolve amongst duplicates by picking the version associated with the largest clockentry

---
+++ src/java/voldemort/store/bdb/dataconversion/BdbFixVectorClocks.java
@@ -0,0 +1,261 @@
+package voldemort.store.bdb.dataconversion;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import org.apache.log4j.Logger;
+
+import voldemort.client.ClientConfig;
+import voldemort.client.protocol.admin.AdminClient;
+import voldemort.client.protocol.admin.AdminClientConfig;
+import voldemort.cluster.Node;
+import voldemort.routing.RoutingStrategy;
+import voldemort.routing.RoutingStrategyFactory;
+import voldemort.store.StoreBinaryFormat;
+import voldemort.store.StoreDefinition;
+import voldemort.utils.CmdUtils;
+import voldemort.versioning.ClockEntry;
+import voldemort.versioning.VectorClock;
+import voldemort.versioning.Version;
+import voldemort.versioning.Versioned;
+import voldemort.xml.StoreDefinitionsMapper;
+
+import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.LockMode;
+import com.sleepycat.je.OperationStatus;
+
+public class BdbFixVectorClocks extends AbstractBdbConversion {
+
+    private final AdminClient adminClient;
+    private final RoutingStrategyFactory factory;
+    private final RoutingStrategy storeRoutingStrategy;
+
+    private final StoreDefinitionsMapper storeDefMapper;
+    private StoreDefinition storeDef = null;
+    static Logger logger = Logger.getLogger(BdbFixVectorClocks.class);
+
+    BdbFixVectorClocks(String storeName,
+                       String storesXmlPath,
+                       String clusterXmlPath,
+                       String sourceEnvPath,
+                       String destEnvPath,
+                       int logFileSize,
+                       int nodeMax) throws Exception {
+        super(storeName, clusterXmlPath, sourceEnvPath, destEnvPath, logFileSize, nodeMax);
+
+        factory = new RoutingStrategyFactory();
+
+        storeDefMapper = new StoreDefinitionsMapper();
+
+        List<StoreDefinition> storeDefs = storeDefMapper.readStoreList(new File(storesXmlPath));
+
+        for(StoreDefinition storeDefInstance: storeDefs) {
+            if(storeDefInstance.getName().equals(storeName)) {
+                storeDef = storeDefInstance;
+                break;
+            }
+        }
+        if(storeDef == null) {
+
+            System.err.println("Store name not found");
+            System.exit(0);
+        }
+
+        adminClient = new AdminClient(cluster, new AdminClientConfig(), new ClientConfig());
+        storeRoutingStrategy = factory.updateRoutingStrategy(storeDef,
+                                                             adminClient.getAdminClientCluster());
+    }
+
+    @Override
+    public void transfer() throws Exception {
+        cursor = srcDB.openCursor(null, null);
+        DatabaseEntry keyEntry = new DatabaseEntry();
+        DatabaseEntry valueEntry = new DatabaseEntry();
+
+        List<Versioned<byte[]>> vals;
+        long startTime = System.currentTimeMillis();
+        int scanCount = 0;
+        int keyCount = 0;
+        while(cursor.getNext(keyEntry, valueEntry, LockMode.READ_UNCOMMITTED) == OperationStatus.SUCCESS) {
+            keyCount++;
+
+            vals = StoreBinaryFormat.fromByteArray(valueEntry.getData());
+            scanCount += vals.size();
+
+            DatabaseEntry winningValueEntry = getWinningValueEntry(storeRoutingStrategy,
+                                                                   keyEntry,
+                                                                   valueEntry);
+
+            OperationStatus putStatus = dstDB.put(null, keyEntry, // write
+                                                                  // the
+                                                                  // original
+                                                                  // key
+                                                                  // without
+                                                                  // striping
+                                                  winningValueEntry); // write
+                                                                      // the
+                                                                      // winning
+                                                                      // value
+
+            if(OperationStatus.SUCCESS != putStatus) {
+                String errorStr = "Put failed with " + putStatus + " for key"
+                                  + BdbConvertData.writeAsciiString(keyEntry.getData());
+                logger.error(errorStr);
+                throw new Exception(errorStr);
+            }
+
+            if(scanCount % 1000000 == 0)
+                logger.info("Reverted " + scanCount + " entries in "
+                            + (System.currentTimeMillis() - startTime) / 1000 + " secs");
+        }
+        logger.info("Reverted " + scanCount + " entries and " + keyCount + " keys in "
+                    + (System.currentTimeMillis() - startTime) / 1000 + " secs");
+    }
+
+    public static DatabaseEntry getWinningValueEntry(RoutingStrategy storeRoutingStrategy,
+                                                     DatabaseEntry keyEntry,
+                                                     DatabaseEntry valueEntry) {
+
+        DatabaseEntry winningValueEntry = new DatabaseEntry();
+
+        // pull out the real key
+        byte[] stripedKey = StoreBinaryFormat.extractKey(keyEntry.getData());
+
+        byte[] winningValue = null;
+        long winningVersion = 0L;
+        long winningTimeStamp = System.currentTimeMillis();
+        // pick the one value with the biggest clockentry
+        // for every versioned value associated with the key find the value
+        // with greatest clockentry
+        for(Versioned<byte[]> val: StoreBinaryFormat.fromByteArray(valueEntry.getData())) {
+            VectorClock version = (VectorClock) val.getVersion();
+            List<ClockEntry> clockEntries = version.getEntries();
+            for(ClockEntry clockEntry: clockEntries) {
+                if(winningVersion < clockEntry.getVersion()) {
+                    winningVersion = clockEntry.getVersion();
+                    winningValue = val.getValue();
+                    winningTimeStamp = version.getTimestamp();
+                    break; // break out of inner loop minor optimization
+                }
+            }
+
+        }
+
+        List<Versioned<byte[]>> values = new ArrayList<Versioned<byte[]>>();
+        values.add(Versioned.value((winningValue),
+                                   generateVectorClock(storeRoutingStrategy,
+                                                       stripedKey,
+                                                       winningVersion,
+                                                       winningTimeStamp)));
+        byte[] winningValueVersionedBytes = StoreBinaryFormat.toByteArray(values);
+
+        winningValueEntry.setData(winningValueVersionedBytes);
+
+        return winningValueEntry;
+
+    }
+
+    public static Version generateVectorClock(RoutingStrategy storeRoutingStrategy,
+                                              byte[] keyBytes,
+                                              Long version,
+                                              Long timestamp) {
+        List<ClockEntry> clockEntries = new ArrayList<ClockEntry>();
+
+        List<Node> nodeList = storeRoutingStrategy.routeRequest(keyBytes);
+
+        // sort the nodes
+        Collections.sort(nodeList);
+
+        for(Node node: nodeList) {
+            clockEntries.add(new ClockEntry((short) node.getId(), version));
+        }
+        return new VectorClock(clockEntries, timestamp);
+    }
+
+    @Override
+    public void close() {
+        super.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        OptionParser parser = new OptionParser();
+        parser.accepts("cluster-xml", "[REQUIRED] path to cluster.xml file for the server")
+              .withRequiredArg()
+              .describedAs("cluster-xml")
+              .ofType(String.class);
+        parser.accepts("stores-xml", "[REQUIRED] path to stores.xml file for the server")
+              .withRequiredArg()
+              .describedAs("stores-xml")
+              .ofType(String.class);
+        parser.accepts("src", "[REQUIRED] Source environment to be converted")
+              .withRequiredArg()
+              .describedAs("source-env")
+              .ofType(String.class);
+        parser.accepts("dest", "[REQUIRED] Destination environment to place converted data into")
+              .withRequiredArg()
+              .describedAs("destination-env")
+              .ofType(String.class);
+        parser.accepts("store", "[REQUIRED] Store/BDB database to convert")
+              .withRequiredArg()
+              .describedAs("store")
+              .ofType(String.class);
+        parser.accepts("je-log-size", "[Optional] Size of the converted JE log files")
+              .withRequiredArg()
+              .describedAs("je-log-size")
+              .ofType(Integer.class);
+        parser.accepts("btree-nodemax", "[Optional] Fanout of converted Btree nodes")
+              .withRequiredArg()
+              .describedAs("btree-nodemax")
+              .ofType(Integer.class);
+
+        OptionSet options = parser.parse(args);
+
+        if(!options.has("cluster-xml") || !options.has("src") || !options.has("dest")
+           || !options.has("store") || !options.has("stores-xml")) {
+            parser.printHelpOn(System.err);
+            System.exit(0);
+        }
+
+        String clusterXmlPath = CmdUtils.valueOf(options, "cluster-xml", null);
+        String storesXmlPath = CmdUtils.valueOf(options, "stores-xml", null);
+        String sourceEnvPath = CmdUtils.valueOf(options, "src", null);
+        String destEnvPath = CmdUtils.valueOf(options, "dest", null);
+        String storeName = CmdUtils.valueOf(options, "store", null);
+
+        Integer logFileSize = CmdUtils.valueOf(options, "je-log-size", 60);
+        Integer nodeMax = CmdUtils.valueOf(options, "btree-nodemax", 512);
+
+        BdbFixVectorClocks clockFixer = new BdbFixVectorClocks(storeName,
+                                                               storesXmlPath,
+                                                               clusterXmlPath,
+                                                               sourceEnvPath,
+                                                               destEnvPath,
+                                                               logFileSize,
+                                                               nodeMax);
+
+        try {
+            clockFixer.transfer();
+        } catch(Exception e) {
+            logger.error("Error converting data", e);
+        } finally {
+            if(clockFixer != null)
+                clockFixer.close();
+        }
+
+    }
+
+    @Override
+    public boolean areDuplicatesNeededForSrc() {
+        return false;
+    }
+
+    @Override
+    public boolean areDuplicatesNeededForDest() {
+        return false;
+    }
+}
---
+++ test/unit/voldemort/store/bdb/dataconversion/BdbFixVectorClocksTest.java
@@ -0,0 +1,120 @@
+package voldemort.store.bdb.dataconversion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import voldemort.cluster.Node;
+import voldemort.routing.RoutingStrategy;
+import voldemort.serialization.StringSerializer;
+import voldemort.store.StoreBinaryFormat;
+import voldemort.versioning.ClockEntry;
+import voldemort.versioning.VectorClock;
+import voldemort.versioning.Versioned;
+
+import com.sleepycat.je.DatabaseEntry;
+
+public class BdbFixVectorClocksTest {
+
+    @Test
+    public void getWinningValueEntryTest() {
+
+        StringSerializer strSer = new StringSerializer();
+        Short nodes1[] = { 1, 3, 5 };
+        Short nodes2[] = { 1, 2, 3, 4, 5 };
+        Short nodes3[] = { 1, 3, 5 };
+        Short nodes4[] = { 3, 5, 1 };
+
+        long timestamp = System.currentTimeMillis();
+
+        long version1 = 20130501203L;
+        long version2 = 20130501204L;
+        long version3 = 20130501206L; // winning version
+        long version4 = 20130501205L;
+
+        String key = "key";
+        String valueStr1 = "version1";
+        String valueStr2 = "version2";
+        String valueStr3 = "version3";
+        String valueStr4 = "version4";
+
+        List<Versioned<byte[]>> values = new ArrayList<Versioned<byte[]>>();
+
+        List<ClockEntry> clockEntries = new ArrayList<ClockEntry>();
+        for(short node: Arrays.asList(nodes1)) {
+            clockEntries.add(new ClockEntry(node, version1));
+        }
+        VectorClock vc1 = new VectorClock(clockEntries, timestamp);
+
+        values.add(Versioned.value((strSer.toBytes(valueStr1)), vc1));
+
+        // add second duplicate
+        clockEntries = new ArrayList<ClockEntry>();
+        for(short node: Arrays.asList(nodes1)) {
+            clockEntries.add(new ClockEntry(node, version2));
+        }
+        VectorClock vc2 = new VectorClock(clockEntries, timestamp);
+
+        values.add(Versioned.value((strSer.toBytes(valueStr2)), vc2));
+
+        // add 3rd duplicate
+        clockEntries = new ArrayList<ClockEntry>();
+        for(short node: Arrays.asList(nodes1)) {
+            clockEntries.add(new ClockEntry(node, version3));
+        }
+        VectorClock vc3 = new VectorClock(clockEntries, timestamp);
+
+        values.add(Versioned.value((strSer.toBytes(valueStr3)), vc3));
+
+        // add 4th duplicate
+        clockEntries = new ArrayList<ClockEntry>();
+        for(short node: Arrays.asList(nodes1)) {
+            clockEntries.add(new ClockEntry(node, version4));
+        }
+        VectorClock vc4 = new VectorClock(clockEntries, timestamp);
+
+        values.add(Versioned.value((strSer.toBytes(valueStr4)), vc4));
+
+        DatabaseEntry keyEntry = new DatabaseEntry();
+        DatabaseEntry valueEntry = new DatabaseEntry();
+        keyEntry.setData(strSer.toBytes(key));
+
+        byte[] valueVersionedBytes = StoreBinaryFormat.toByteArray(values);
+
+        valueEntry.setData(valueVersionedBytes);
+
+        RoutingStrategy storeRoutingStrategy = Mockito.mock(RoutingStrategy.class);
+        Node node1 = Mockito.mock(Node.class);
+        Node node2 = Mockito.mock(Node.class);
+        Node node3 = Mockito.mock(Node.class);
+
+        Mockito.when(node1.getId()).thenReturn(1);
+        Mockito.when(node2.getId()).thenReturn(3);
+        Mockito.when(node3.getId()).thenReturn(5);
+
+        List<Node> nodeList = new ArrayList();
+        nodeList.add(node1);
+        nodeList.add(node2);
+        nodeList.add(node3);
+
+        Mockito.when(storeRoutingStrategy.routeRequest(Mockito.any(byte[].class)))
+               .thenReturn(nodeList);
+
+        DatabaseEntry winningEntry = BdbFixVectorClocks.getWinningValueEntry(storeRoutingStrategy,
+                                                                             keyEntry,
+                                                                             valueEntry);
+        List<Versioned<byte[]>> returnvalues = StoreBinaryFormat.fromByteArray(winningEntry.getData());
+
+        Assert.assertEquals(returnvalues.size(), 1);
+
+        byte[] winningValue = returnvalues.get(0).getValue();
+
+        String winningStr = strSer.toObject(winningValue);
+
+        Assert.assertEquals(winningStr, valueStr3);
+    }
+}
